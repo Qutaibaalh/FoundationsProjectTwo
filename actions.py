9e5add1a558caf0ebdeb362d1ff3e95138d5c0a5
qutaibaalh <qutaibaalh@icloud.com>
2018-06-25 19:13:43 +0300

    submission

--- actions.py
+++ actions.py
@@ -2,28 +2,25 @@
 from data import stores
 from components import Cart
 
-site_name = ""  # Give your site a name
+site_name = "www.LazyShop.com"  
 
 def welcome():
-    print("Welcome to %s\nFeel free to shop throughout the stores we have, and only checkout once!" % site_name)
+    print("Welcome to %s\nFeel free to shop throughout the stores we have, and only checkout once!" % www.LazyShop.com)
 
 def print_stores():
-    """
-    prints the list of stores in a nice readable format.
-    """
-    # your code goes here!
+    for store in stores 
+        print ("- %s" % store.name)
 
 def get_store(store_name):
-    """
-    receives a name for a store, and returns the store object with that name.
-    """
-    # your code goes here!
+    for store in stores 
+        if store.name == store_name:
+            return store 
+        else: 
+            return False 
 
 def pick_store():
-    """
-    prints list of stores and prompts user to pick a store.
-    """
-    # your code goes here!
+    print_stores()
+    print ()
 
 def pick_products(cart, picked_store):
     """
--- components.py
+++ components.py
@@ -1,59 +1,44 @@
 # CLASSES AND METHODS
 class Store():
     def __init__(self, name):
-        """
-        Initializes a new store with a name.
-        """
-        # your code goes here!
+       self.name = name
+       self.products =  []
 
     def add_product(self, product):
-        """
-        Adds a product to the list of products in this store.
-        """
-        # your code goes here!
+        self.products.append(product)
+        
 
     def print_products(self):
-        """
-        Prints all the products of this store in a nice readable format.
-        """
-        # your code goes here!
+        print self.products
 
 
 class Product():
     def __init__(self, name, description, price):
-        """
-        Initializes a new product with a name, a description, and a price.
-        """
-        # your code goes here!
+        self.name = name 
+        self.description = description 
+        self.price = price 
 
     def __str__(self):
-        # your code goes here!
+        return "Product Name: %s\n product Description: %s\nPrice: %s KWD" %(self.name, self.description, self.price)
 
 
 class Cart():
     def __init__(self):
-        """
-        Initializes a new cart with an empty list of products.
-        """
-        # your code goes here!
+       self.products = []
 
     def add_to_cart(self, product):
-        """
-        Adds a product to this cart.
-        """
-        # your code goes here!
+        self.products.append(product)
 
     def get_total_price(self):
-        """
-        Returns the total price of all the products in this cart.
-        """
-        # your code goes here!
+       price = 0 
+       for product in self.products
+            price += product.price
+            return price 
 
     def print_receipt(self):
-        """
-        Prints the receipt in a nice readable format.
-        """
-        # your code goes here!
+        for products in add_to_cart 
+            print (product) 
+            print (get_total_price())
 
     def checkout(self):
         """
